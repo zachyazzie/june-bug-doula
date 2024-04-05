538d70405fb296ccb418be659e5ce984f77025ca
Zach Yazzie <zacharyyazzie94@gmail.com>
2024-04-05 00:47:02 -0600

    continuing work on page

--- src/dist/site.dev.js
+++ src/dist/site.dev.js
@@ -1,12 +1,11 @@
+"use strict";
+
 $(document).ready(function () {
   $(document).scroll(function () {
     var scrolledHeight = $(window).scrollTop();
     var parallaxSpeed = 0.5; // Adjust for faster or slower parallax effect
-
     // Adjust background position for reverse effect
-    $("#title-section-bg-image").css(
-      "background-position-y",
-      scrolledHeight * parallaxSpeed + "px"
-    );
+
+    $("#title-section-bg-image").css("background-position-y", scrolledHeight * parallaxSpeed + "px");
   });
 });
